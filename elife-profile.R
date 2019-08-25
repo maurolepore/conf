54e7ab59497b161701f3b27d476dc9776f884f76
maurolepore <maurolepore@gmail.com>
2019-08-25 04:46:47 -0500

    Structure rstudio application

---
+++ elife-profile.R
@@ -0,0 +1,52 @@
+First name
+Mauro
+Last name
+Lepore
+
+Job title, Organisation (optional)
+* Software developer (2degrees-investing.org).
+* Occational sofware reviewer (rOpenSci), and data science instructor (carpentries.org), and consultant (ixpantia.com).
+
+
+Put in a profile pic
+Dropbox/Temp/images_common
+
+Interests?
+
+Research
+Data
+Software development
+Training
+Open
+Collaboration
+Reuse
+Transparent
+Diversity
+Accessibility
+Fun
+Easy of use
+Human centric
+teaching
+reproducible
+community
+R
+R package
+Continuous integration
+Verison control
+
+
+Technologies I use daily include R, RStudio, Git, GitHub, and TravisCI.
+
+
+I regularly contribute to open software and reproducible research (https://maurolepore.github.io). For example, I am an instructor for The Carpentries (https://carpentries.org/instructors/), and a peer-reviewer of rOpenSci packages (http://bit.ly/mauro-ropensci-review-gitignore). Also, I have been invited to attend other open science meetings (https://unconf18.ropensci.org/#participants, and https://chirunconf.github.io/#team), and to speak at a DataLatam webinar (http://youtu.be/SVzyWV1Iep8).
+
+
+
+
+
+
+
+
+
+Contribute?
+ask/need?
