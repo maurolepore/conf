1a37ea484d200dc8ea9f070bd678eb354311f82a
maurolepore <maurolepore@gmail.com>
2019-08-27 02:04:06 -0500

    Draft profile

--- elife-profile.R
+++ elife-profile.R
@@ -1,52 +1,73 @@
+### Name
+
 First name
 Mauro
 Last name
 Lepore
 
-Job title, Organisation (optional)
+### Job title, Organisation (optional)
+
 * Software developer (2degrees-investing.org).
-* Occational sofware reviewer (rOpenSci), and data science instructor (carpentries.org), and consultant (ixpantia.com).
+* Consultant (ixpantia.com).
+* Data science instructor (carpentries.org).
+* Active in the R community (e.g. rOpenSci, RLadies).
 
+### Put in a profile pic
 
-Put in a profile pic
 Dropbox/Temp/images_common
 
-Interests?
+### Interests?
 
-Research
-Data
-Software development
-Training
-Open
-Collaboration
-Reuse
-Transparent
-Diversity
-Accessibility
-Fun
-Easy of use
-Human centric
-teaching
-reproducible
-community
-R
-R package
-Continuous integration
-Verison control
+What?
+
+* Teaching/learning.
+* Sofware development.
+* Research.
+
+How?
+
+* Accesible.
+* Diverse.
+* Transparent.
+* Collaborative.
+
+* Systematic ("Don't reinvent the wheels"):
+    * "We don't rise to the level of our goals; we fall to the level of our systems".
+      --?
+    * "We are what we repeatedly do".
+      -- Aristotle?
 
 
-Technologies I use daily include R, RStudio, Git, GitHub, and TravisCI.
+* Getting along with people, Human behaviour (behavioural economics?), Healty habits
+* Nature
 
 
-I regularly contribute to open software and reproducible research (https://maurolepore.github.io). For example, I am an instructor for The Carpentries (https://carpentries.org/instructors/), and a peer-reviewer of rOpenSci packages (http://bit.ly/mauro-ropensci-review-gitignore). Also, I have been invited to attend other open science meetings (https://unconf18.ropensci.org/#participants, and https://chirunconf.github.io/#team), and to speak at a DataLatam webinar (http://youtu.be/SVzyWV1Iep8).
+Getting along with people
+* Do meaningful, fun work
 
-
-
-
-
+Human centric (Goodness by design, stuff that is easy of use)
 
 
 
 
 Contribute?
+
+* Software development (R packages).
+* Version control (Git + GitHub).
+* Continuous integration (TravisCI).
+
 ask/need?
+
+Goal: Can we create a system to more easily and effectively onboard new commers to open science/research?
+
+* People who want both to improve something and have fun doing it (__growth and kindness mindset__). By donating our time we are all making a favour to each other. How would you communicate with someone who is doing a favour to you?
+
+* People with experience in __design thinking__ (building/testing prototype)
+
+* People to __research__ on potential approaches (Carpentries, rOpenSci, Greg Wilson).
+  * What are the elements of a successfull onboarding process?
+      * Community? People with experience/interest __building community__.
+      * Elegance? People with experience/interest in __editting__ or giving constructive __peer-review__.
+      * Free? People with experience/interest in __open source__.
+
+* Maybe people to develop and answer a survey.
